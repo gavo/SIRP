39ac035f81ac03bd06f3bad80ae39a92419a7047
gavo <gvmgust@hotmail.com>
2013-05-06 13:32:15 -0400

    Agregando Librerias necesarias para el Funcionamiento del Programa

--- src/sirp/SIRP.java
+++ src/sirp/SIRP.java
@@ -5,6 +5,8 @@ public class SIRP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        con = new Conexion("root","","localhost");
     }
+    
+    public static Conexion con;
 }
