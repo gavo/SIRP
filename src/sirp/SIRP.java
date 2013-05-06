461033f298d9e13dfdec46657aee5cf273cc1b9f
Gavo <gvmgust@gmail.com>
2013-05-06 17:01:53 -0400

    Agregando Look And Feel al proyecto (estilo Liquid)

--- src/sirp/SIRP.java
+++ src/sirp/SIRP.java
@@ -1,12 +1,29 @@
 package sirp;
+import com.birosoft.liquid.LiquidLookAndFeel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UnsupportedLookAndFeelException;
 public class SIRP {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         con = new Conexion("root","","localhost");
     }
     
     public static Conexion con;
+    
+    public static void style(){
+        try {
+            javax.swing.UIManager.setLookAndFeel("com.birosoft.liquid.LiquidLookAndFeel");
+            LiquidLookAndFeel.setLiquidDecorations(true);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SIRP.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(SIRP.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(SIRP.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(SIRP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+  
+   }
 }
