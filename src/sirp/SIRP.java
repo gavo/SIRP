22a1005d8086c764c445d512f314bf2d0f8e1b10
Gavo <gvmgust@gmail.com>
2013-05-06 11:22:23 -0400

    Inicio del Proyecto

---
+++ src/sirp/SIRP.java
@@ -0,0 +1,10 @@
+package sirp;
+public class SIRP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
